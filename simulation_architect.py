1d20782254be2b1304ae36bc81483bc14cfcf2c6
Maurice Ling <mauriceling@acm.org>
2021-08-16 17:49:54 +0800

    # combine into trial_architect.py

--- simulation_architect.py
+++
@@ -1,9 +0,0 @@
-"""!
-ZeroOne (also known as Machine City) - Architect Code
-
-Date created: 15th August 2021
-"""
-archCode = {}
-archEnv = {}
-loop_delay = 10
-simulation_maintenance = False
---
+++ trial_architect.py
@@ -0,0 +1,25 @@
+"""!
+ZeroOne (also known as Machine City) - Trial Architect Code
+
+Date created: 15th August 2021
+"""
+def architect(program, environment):
+    return environment
+architectE = {'identity': 'architect',
+              'passcode': 'iamtheboss'}
+
+def oracle(program, environment):
+    return environment
+oracleE = {'identity': 'oracle',
+              'passcode': 'ilovefood'}
+
+archCode = \
+{0: architect,
+1: oracle}
+
+archEnv = \
+{0: architectE,
+1: oracleE}
+
+loop_delay = 10
+simulation_maintenance = True
